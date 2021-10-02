06dfe8bcc205f866e6e47117c0cb4ea8824aab69
eric-prog <eric.sprog@gmail.com>
2021-10-01 20:30:47 -0400

    Completely organized detail page

--- js/detail.js
+++ js/detail.js
@@ -1,17 +1,25 @@
 function treeData (data_link) {
-    // set the dimensions and margins of the diagram
-    var margin = {top: 10, right: 570, bottom: 10, left: 570},
-    width = 300,
-    height = 200 - margin.top - margin.bottom - 50;
-    
-    var treemap = d3.tree()
-        .size([height, width]);
-
     d3.json(data_link).then(function(treeData) {
-        for (let z = 0; z < treeData["data"].length; z++) {
+        for (let z = 0; z < treeData["data"].length; z++) { 
+            var margin = {top: 10, right: 570, bottom: 10, left: 570},
+                width = 300,
+                height = 180;
+
+            var _data = treeData["data"][z]
+
+            if (_data.children.length >= 5) {
+                margin = {top: 30, right: 570, bottom: 30, left: 570},
+                width = 300,
+                height = 280; 
+            }
+            
+            console.log(_data)
+
+            var treemap = d3.tree()
+                .size([height, width]);
 
             //  assigns the data to a hierarchy using parent-child relationships
-            var nodes = d3.hierarchy(treeData["data"][z], function(d) {
+            var nodes = d3.hierarchy(_data, function(d) {
                 return d.children;
             });
 
@@ -29,18 +37,18 @@ function treeData (data_link) {
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
             svg.append('defs').append('marker')
-                    .attr("id",'arrowhead')
-                    .attr('viewBox','-0 -5 10 10') //the bound of the SVG viewport for the current SVG fragment. defines a coordinate system 10 wide and 10 high starting on (0,-5)
-                    .attr('refX',23) // x coordinate for the reference point of the marker. If circle is bigger, this need to be bigger.
-                    .attr('refY',0)
-                    .attr('orient','auto')
-                    .attr('markerWidth',10)
-                    .attr('markerHeight',10)
-                    .attr('xoverflow','visible')
-                    .append('svg:path')
-                    .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
-                    .attr('fill', '#000')
-                    .style('stroke','none')
+                .attr("id",'arrowhead')
+                .attr('viewBox','-0 -5 10 10') //the bound of the SVG viewport for the current SVG fragment. defines a coordinate system 10 wide and 10 high starting on (0,-5)
+                .attr('refX',23) // x coordinate for the reference point of the marker. If circle is bigger, this need to be bigger.
+                .attr('refY',0)
+                .attr('orient','auto')
+                .attr('markerWidth',10)
+                .attr('markerHeight',10)
+                .attr('xoverflow','visible')
+                .append('svg:path')
+                .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+                .attr('fill', '#000')
+                .style('stroke','none')
 
             // adds the links between the nodes
             var link = g.selectAll(".link")
@@ -64,7 +72,7 @@ function treeData (data_link) {
                     (d.children ? " node--internal" : " node--leaf"); })
                 .attr("transform", function(d) { 
                 return "translate(" + d.y + "," + d.x + ")"; })
-    
+
             // adds the circle to the node
             node.append("circle")
             .attr("r", 7)
@@ -80,11 +88,46 @@ function treeData (data_link) {
 
             // adds the text to the node
             node.append("text")
-            .attr("dy", ".35em")
+            .attr("dy", ".36em")
             .attr("x", function(d) { return d.children ? -13 : 13; })
             .style("text-anchor", function(d) { 
                 return d.children ? "end" : "start"; })
-            .style("font-size", 8)
+            .style("font-size", 12)
             .text(function(d) { return d.data.name; })
+            .call(wrap, 480);
         }})
+}
+
+
+function wrap(text, width) {
+    text.each(function () {
+        var text = d3.select(this),
+            words = text.text().split(/\s+/).reverse(),
+            word,
+            line = [],
+            lineNumber = 0,
+            lineHeight = 1.1, // ems
+            x = text.attr("x"),
+            y = text.attr("y"),
+            dy = 0, //parseFloat(text.attr("dy")),
+            tspan = text.text(null)
+                        .append("tspan")
+                        .attr("x", x)
+                        .attr("y", y + 3)
+                        .attr("dy", dy + "em");
+        while (word = words.pop()) {
+            line.push(word);
+            tspan.text(line.join(" "));
+            if (tspan.node().getComputedTextLength() > width) {
+                line.pop();
+                tspan.text(line.join(" "));
+                line = [word];
+                tspan = text.append("tspan")
+                            .attr("x", x)
+                            .attr("y", y)
+                            .attr("dy", ++lineNumber * lineHeight + dy + "em")
+                            .text(word)
+            }
+        }
+    });
 }
