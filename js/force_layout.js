dac8f246063a19d0d05108a30bdac4fe6da11bf9
eric-prog <eric.sprog@gmail.com>
2021-10-15 16:02:57 -0400

    Modality metrics and colors complete

--- js/force_layout.js
+++ js/force_layout.js
@@ -3,8 +3,8 @@ function force_layout(data_link) {
     const width =  890 - margin.left - margin.right
     const height=  800 - margin.top - margin.bottom
     const colorScale  = d3.scaleOrdinal() //=d3.scaleOrdinal(d3.schemeSet2)
-        .domain(["Team C", "Team D", "Team E", "Team F", "Team G", "Team H"])
-        .range(['#ffbfff','#a3ffab','#ffcba6', '#feffc0', '#ffd4dc', '#b5ffff'])
+        .domain(["Team C", "Team D", "Team E", "Team F", "Team G", "Team H", "Team I"])
+        .range(['#ffbfff','#a3ffab','#ffcba6', '#feffc0', '#ffd4dc', '#b5ffff', '#f5f2f2'])
 
     function drag(simulation) {
     
@@ -63,118 +63,136 @@ function force_layout(data_link) {
         .attr('fill','rgb(104, 104, 104)');
     //create some data
 
-    d3.json(data_link)
-    .then(function(dataset){
-        // Initialize the links
-        const link = svg.selectAll(".links")
-            .data(dataset.links)
-            .enter()
-            .append("line")
-            .attr('class', 'links')
-            .style("stroke", "rgb(104, 104, 104)")
-            // .style("stroke-dasharray", d => {return d.linetype})
-            .style("stroke-width", d => {return d.linelevel})
-            .attr('marker-end','url(#arrowhead)') 
-        link.append("title")
-            .text(d => d.linelevel);
-        // const edgepaths = svg.selectAll(".edgepath") //make path go along with the link provide position for link labels
-        //     .data(dataset.links)
-        //     .enter()
-        //     .append('path')
-        //     .attr('class', 'edgepath')
-        //     .attr('fill-opacity', 0)
-        //     .attr('stroke-opacity', 0)
-        //     .attr('id', function (d, i) {return 'edgepath' + i})
-        //     .style("pointer-events", "none");
-        // const edgelabels = svg.selectAll(".edgelabel")
-        //     .data(dataset.links)
-        //     .enter()
-        //     .append('text')
-        //     .style("pointer-events", "none")
-        //     .attr('class', 'edgelabel')
-        //     .attr('id', function (d, i) {return 'edgelabel' + i})
-        //     .attr('font-size', 10)
-        //     .attr('fill', '#aaa')
-        // edgelabels.append('textPath') //To render text along the shape of a <path>, enclose the text in a <textPath> element that has an href attribute with a reference to the <path> element.
-        //     .attr('xlink:href', function (d, i) {return '#edgepath' + i})
-        //     .style("text-anchor", "middle")
-        //     .style("pointer-events", "none")
-        //     .attr("startOffset", "50%")
-        //     .text(d => d.type);
-        // Initialize the nodes
-        const node = svg.selectAll(".nodes")
-            .data(dataset.nodes)
-            .enter()
-            .append("g") 
-            .attr("class", "nodes")
-            .call(drag(simulation))
-            .append("a")
-            .on('click', function(i, d) {
-            if(d.url != "parent"){
-                window.open(
-                    "https://scholar.google.com/scholar?hl=en&q=" + d.url,
-                    '_blank' 
-                );
-            } else {
-                resetVis();
-            }})
-            .style("cursor", "pointer"); 
+    d3.json(data_link) .then(function(dataset){
+        d3.json("/data/data_color.json").then(function(data){
+            // Initialize the links
+            const link = svg.selectAll(".links")
+                .data(dataset.links)
+                .enter()
+                .append("line")
+                .attr('class', 'links')
+                .style("stroke", "rgb(104, 104, 104)")
+                // .style("stroke-dasharray", d => {return d.linetype})
+                .style("stroke-width", d => {return d.linelevel})
+                .attr('marker-end','url(#arrowhead)') 
+            link.append("title")
+                .text(d => d.linelevel);
+            // const edgepaths = svg.selectAll(".edgepath") //make path go along with the link provide position for link labels
+            //     .data(dataset.links)
+            //     .enter()
+            //     .append('path')
+            //     .attr('class', 'edgepath')
+            //     .attr('fill-opacity', 0)
+            //     .attr('stroke-opacity', 0)
+            //     .attr('id', function (d, i) {return 'edgepath' + i})
+            //     .style("pointer-events", "none");
+            // const edgelabels = svg.selectAll(".edgelabel")
+            //     .data(dataset.links)
+            //     .enter()
+            //     .append('text')
+            //     .style("pointer-events", "none")
+            //     .attr('class', 'edgelabel')
+            //     .attr('id', function (d, i) {return 'edgelabel' + i})
+            //     .attr('font-size', 10)
+            //     .attr('fill', '#aaa')
+            // edgelabels.append('textPath') //To render text along the shape of a <path>, enclose the text in a <textPath> element that has an href attribute with a reference to the <path> element.
+            //     .attr('xlink:href', function (d, i) {return '#edgepath' + i})
+            //     .style("text-anchor", "middle")
+            //     .style("pointer-events", "none")
+            //     .attr("startOffset", "50%")
+            //     .text(d => d.type);
+            // Initialize the nodes
+            const node = svg.selectAll(".nodes")
+                .data(dataset.nodes)
+                .enter()
+                .append("g") 
+                .attr("class", "nodes")
+                .call(drag(simulation))
+                .append("a")
+                .on('click', function(i, d) {
+                if(d.url != "parent"){
+                    window.open(
+                        "https://scholar.google.com/scholar?hl=en&q=" + d.url,
+                        '_blank' 
+                    );
+                } else {
+                    resetVis();
+                }})
+                .style("cursor", "pointer"); 
 
-            
-        // node.append("circle")
-        //     .attr("r", d=> 20)//+ d.runtime/20 )
-        //     .style("stroke", "grey")
-        //     .style("stroke-opacity",0.3)
-        //     .style("stroke-width", d => d.runtime/10)
-        //     .style("fill", d => colorScale(d.group)) 
-        node.append("rect")
-            .attr("height", 22)//+ d.runtime/20 )
-            .attr("y", -10)
-            .attr("x", -15 - 8)
-            .style("fill", d => colorScale(d.group))
-        node.append("text")
-            .attr("dy", 4)
-            .attr("dx", -15)
-            .text(d => d.name)
-        node.append("title")
-            .text(d => d.name);
-        node.selectAll('rect')
-            .attr("rx", 6)
-            .attr("ry", 6)
-            .attr("width", function(d) {return this.parentNode.getBBox().width + 15;})            
+                
+            // node.append("circle")
+            //     .attr("r", d=> 20)//+ d.runtime/20 )
+            //     .style("stroke", "grey")
+            //     .style("stroke-opacity",0.3)
+            //     .style("stroke-width", d => d.runtime/10)
+            //     .style("fill", d => colorScale(d.group)) 
+            node.append("rect")
+                .attr("height", 22)//+ d.runtime/20 )
+                .attr("y", -10)
+                .attr("x", -15 - 8)
+                .style("fill", function(d){
+                    if(d.url == "parent") {
+                        return colorScale("Team I")
+                    }
+                    
+                    let color_dict = data["color_dict"]
+                    let color_type = color_dict[d.name]
 
-        // node.append("text")
-        //     .attr("dy",12)
-        //     .attr("dx", -8)
-        //     .text(d => d.runtime);
+                    if(data_link.includes("layer_1")) {
+                        return colorScale(d.group)
+                    }
 
-        //Listen for tick events to render the nodes as they update in your Canvas or SVG.
-        simulation
-            .nodes(dataset.nodes)
-            .on("tick", ticked);
-        simulation.force("link")
-            .links(dataset.links);
-        // This function is run at each iteration of the force algorithm, updating the nodes position (the nodes data array is directly manipulated).
-        function ticked() {
-            link.attr("x1", d => d.source.x)
-                .attr("y1", d => d.source.y)
-                .attr("x2", d => d.target.x)
-                .attr("y2", d => d.target.y);
-            node.attr("transform", d => `translate(${d.x},${d.y})`);
-            // edgepaths.attr('d', d => 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y);
-        }
-        //When the drag gesture starts, the targeted node is fixed to the pointer
-        //The simulation is temporarily "heated" during interaction by setting the target alpha to a non-zero value.
-        function dragstarted(d) {
-            if (!d3.event.active) simulation.alphaTarget(0.3).restart();//sets the current target alpha to the specified number in the range [0,1].
-            d.fy = d.y; //fx - the node's fixed x-position. Original is null.
-            d.fx = d.x; //fy - the node's fixed y-position. Original is null.
-        }
-        //When the drag gesture starts, the targeted node is fixed to the pointer
-        function dragged(d) {
-            d.fx = d3.event.x;
-            d.fy = d3.event.y;
-        }
+                    if(color_type == undefined) {
+                        console.log("Undefined: " + d.name)
+                    } else {
+                        // console.log(color_dict[d.name])
+                        return colorScale(color_type)
+                    }
+                })
+            node.append("text")
+                .attr("dy", 4)
+                .attr("dx", -15)
+                .text(d => d.name)
+            node.append("title")
+                .text(d => d.name);
+            node.selectAll('rect')
+                .attr("rx", 6)
+                .attr("ry", 6)
+                .attr("width", function(d) {return this.parentNode.getBBox().width + 15;})            
+
+            // node.append("text")
+            //     .attr("dy",12)
+            //     .attr("dx", -8)
+            //     .text(d => d.runtime);
+
+            //Listen for tick events to render the nodes as they update in your Canvas or SVG.
+            simulation
+                .nodes(dataset.nodes)
+                .on("tick", ticked);
+            simulation.force("link")
+                .links(dataset.links);
+            // This function is run at each iteration of the force algorithm, updating the nodes position (the nodes data array is directly manipulated).
+            function ticked() {
+                link.attr("x1", d => d.source.x)
+                    .attr("y1", d => d.source.y)
+                    .attr("x2", d => d.target.x)
+                    .attr("y2", d => d.target.y);
+                node.attr("transform", d => `translate(${d.x},${d.y})`);
+                // edgepaths.attr('d', d => 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y);
+            }
+            //When the drag gesture starts, the targeted node is fixed to the pointer
+            //The simulation is temporarily "heated" during interaction by setting the target alpha to a non-zero value.
+            function dragstarted(d) {
+                if (!d3.event.active) simulation.alphaTarget(0.3).restart();//sets the current target alpha to the specified number in the range [0,1].
+                d.fy = d.y; //fx - the node's fixed x-position. Original is null.
+                d.fx = d.x; //fy - the node's fixed y-position. Original is null.
+            }
+            //When the drag gesture starts, the targeted node is fixed to the pointer
+            function dragged(d) {
+                d.fx = d3.event.x;
+                d.fy = d3.event.y;
+            }
+        })
     })
 }
-
