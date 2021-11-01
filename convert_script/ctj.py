596f39aefe995f37689d79afe10d13ec2ae00406
eric-prog <eric.sprog@gmail.com>
2021-11-01 12:34:41 -0400

    Write to json file

--- convert_script/ctj.py
+++ convert_script/ctj.py
@@ -1,6 +1,28 @@
 # CTJ => Convert to JSON
 
 import csv 
+import json
+
+
+# function to add to JSON
+def write_json(new_data, filename=""):
+    with open(filename,'r+') as file:
+          # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+        file_data["nodes"].append(new_data)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent = 4)
+
+
+y = {"emp_name":"Nikhil",
+     "email": "nikhil@geeksforgeeks.org",
+     "job_profile": "Full Time"
+    }
+     
+write_json(y, "test.json")
 
 
 # List of URLs and there respective names
@@ -238,3 +260,4 @@ def get_data():
         else:
             print("Cannot be identified.")
 get_data()
+
