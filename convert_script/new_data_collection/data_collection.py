38f181326f19443b763e6d70a8484d6ad2cf1894
Andrew Holmes <acholmes001@gmail.com>
2023-02-19 17:26:39 -0500

    Data collection script now pulls from meta file to get title and other meta details. Bug to be fixed: currently doesn't work with empty rows in the CSV file

--- convert_script/new_data_collection/data_collection.py
+++ convert_script/new_data_collection/data_collection.py
@@ -2,7 +2,8 @@ import pandas as pd
 import json
 ''' 
 Parameters:
---> DATA_PATH: location of input data
+--> METRIC_PATH: location of input metric data
+--> META_PATH: location of input meta data
 --> JSON_PATH: desired location/name of output data (must end .JSON)
 --> FINAL_CODED: paper_id_new of final paper that has been completely coded 
 --> ALL_PAPERS: If set to True, data ouptut shold contain all papers (including incompletely coded ones/whatever is in the csv)
@@ -13,9 +14,16 @@ Parameters:
 --> DEBUG_PATH: Currently not implemented
 --> USEFUL_FIELDS: array of fields desired in final JSON result
 --> ARRAY_FIELDS: array of fields that should be converted to an array
+
+IMPORTANT NOTES:
+ - Currently doesn't work with empty lines in the data. Will hopefully fix soon, but for now
+ - have to manually edit CSV files first to ensure all data is contiguous
 '''
 
-DATA_PATH = "data_raw.csv"
+# TODO: Handle input files having empty lines
+
+METRIC_PATH = "data_metrics_constructs.csv"
+META_PATH = "paper_meta.csv"
 JSON_PATH = "data.JSON"
 FINAL_CODED = 74
 ALL_PAPERS = False
@@ -23,7 +31,7 @@ ALL_FIELDS = False
 # DEBUG = False
 # DEBUG_PATH = 'debug.txt'
 USEFUL_FIELDS = [
-    "paper_id_new",
+    "paper_id_ new",
     "data_standardized",
     "sensor",
     "metric", 
@@ -33,7 +41,10 @@ USEFUL_FIELDS = [
     "outcome_larger_category", 
     "outcome_smaller_category",
     "outcome_instrument",
-    "analysis_and_results mm-oo:analysis:resultsig"
+    "analysis_and_results mm-oo:analysis:resultsig",
+    "title",
+    "year",
+    "authors"
 ]
 ARRAY_FIELDS = ["data", 
                 "data_standardized", 
@@ -48,35 +59,40 @@ ARRAY_FIELDS = ["data",
                 "outcome",
                 "outcome_instrument",
                 "outcome_smaller_category",
-                "outcome_larger_category"]
+                "outcome_larger_category",
+                "analysis_and_results mm-oo:analysis:resultsig"]
 
 def CSV_to_JSON():
     # Read in data
-    data = pd.read_csv(DATA_PATH)
+    metric_data = pd.read_csv(METRIC_PATH)
+    meta_data = pd.read_csv(META_PATH)
+    meta_data.astype({"paper_id_new": "int"})
+    metric_data.astype({"paper_id_new": "int"})
+    combined = pd.merge(meta_data, metric_data, how="left", on="paper_id_new")
     # Dropping fields and papers we don't need unless we have ALL_FIELDS or ALL_PAPERS
     if not(ALL_FIELDS):
-        data.drop(list(filter(lambda x: not(x in USEFUL_FIELDS), list(data.columns))), axis="columns", inplace=True)
+        combined.drop(list(filter(lambda x: not(x in USEFUL_FIELDS), list(combined.columns))), axis="columns", inplace=True)
     if not(ALL_PAPERS):
-        data.drop(list(range(FINAL_CODED + 1, data.shape[0])), axis="rows", inplace=True)
+        combined.drop(list(range(FINAL_CODED + 1, combined.shape[0])), axis="rows", inplace=True)
     # Dump to JSON file. Pandas dataframes useful, but we can't convert things to arrays easily
     # which I would like in the JSON as the most sensible represenation of some of the data
-    data.to_json(JSON_PATH)
+    combined.to_json(JSON_PATH)
     # Pulling back from JSON we just wrote
     with open(JSON_PATH, 'r+') as f:
-        data = json.load(f)
-        for field in data:
+        combined = json.load(f)
+        for field in combined:
             # For all the array fields, we will convert the data to arrays
             if field in ARRAY_FIELDS:
-                for entry in data[field]:
-                        data[field][entry] = str(data[field][entry]).split('\n')
+                for entry in combined[field]:
+                        combined[field][entry] = str(combined[field][entry]).split('\n')
         
         # Writing JSON back to file. Reset file pointer and dump data back into file
         f.seek(0)
-        json.dump(data, f, indent=4)
+        json.dump(combined, f, indent=4)
         f.truncate()
     
     # Now taking transpose to organize data by paper
-    data = pd.read_json(JSON_PATH).transpose()
-    data.to_json(JSON_PATH)
+    combined = pd.read_json(JSON_PATH).transpose()
+    combined.to_json(JSON_PATH)
 
 CSV_to_JSON()
